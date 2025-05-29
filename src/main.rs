cba01b79f0d97ce054905c2736aabc7b49cf0dfd
unknown <regime_skylark.0y@icloud.com>
2025-05-29 10:56:52 -0400

    💫CHANGED💫 to using the README example

--- src/main.rs
+++ src/main.rs
@@ -27,10 +27,7 @@ fn main() -> Result<()> {
 
     // Clean some URLs
     let urls = vec![
-        "https://google.com/search?q=rust&utm_source=newsletter&utm_campaign=weekly",
-        "https://www.anglepoise.com/usa/product/type-75-desk-lamp-paul-smith-edition-2/?utm_term=&utm_campaign=Outlet+%7C+Shopping+%7C+US+%7C+Superrb&utm_source=adwords&utm_medium=ppc&hsa_acc=8384491024&hsa_cam=21872127519&hsa_grp=176191620943&hsa_ad=743720003206&hsa_src=g&hsa_tgt=pla-297490888025&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3&gad_source=1&gad_campaignid=21872127519&gbraid=0AAAAADuKdZNlL54l3KzzZgZdECJ7uBv_x&gclid=CjwKCAjw6NrBBhB6EiwAvnT_rgkCnerJW6TdzZqUjJ2RsXkr8vKbsW_N1MRK07tQTW_AqQLcfgQurhoCW3sQAvD_BwE",
-        "https://facebook.com/page?fbclid=abc123&ref=share",
-        "https://example.com/?utm_medium=email&important_param=keep_this",
+        "https://www.amazon.com/dp/exampleProduct/ref=sxin_0_pb?__mk_de_DE=ÅMÅŽÕÑ&keywords=tea&pd_rd_i=exampleProduct&pd_rd_r=8d39e4cd-1e4f-43db-b6e7-72e969a84aa5&pd_rd_w=1pcKM&pd_rd_wg=hYrNl&pf_rd_p=50bbfd25-5ef7-41a2-68d6-74d854b30e30&pf_rd_r=0GMWD0YYKA7XFGX55ADP&qid=1517757263&rnid=2914120011",
     ];
 
     for url in urls {
